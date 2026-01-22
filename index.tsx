720243ea860dc6e1a0528a07d8dcb19498d64e9c
nikomakss <szj6yyqwvf@privaterelay.appleid.com>
2026-01-22 23:46:43 +0000

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+const root = document.getElementById("root");
+if (!root) throw new Error("Missing #root element");
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
