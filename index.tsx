159c45ab26ee2affc9c7e9e8ec9aa6f6bb0a5a58
nikomakss <szj6yyqwvf@privaterelay.appleid.com>
2026-01-22 23:22:32 +0000

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,13 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const container = document.getElementById("root");
-
-if (!container) {
-  throw new Error("Root container missing in index.html");
-}
-
-createRoot(container).render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
