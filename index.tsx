7af7bb0f523ec4d44ce3acec0460f23fb1182bed
nikomakss <szj6yyqwvf@privaterelay.appleid.com>
2026-01-22 23:14:12 +0000

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("Root container missing in index.html");
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+createRoot(container).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
